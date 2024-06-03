d8809b446e4497b9db215bf8538f18c6121a6157
Matteus Moreno <matteus_moreno@live.com>
2024-06-03 19:17:59 -0300

    projeto inicial

---
+++ src/main/java/br/com/matteusmoreno/email_ms/EmailMsApplication.java
@@ -0,0 +1,13 @@
+package br.com.matteusmoreno.email_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/matteusmoreno/email_ms/EmailMsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.matteusmoreno.email_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
